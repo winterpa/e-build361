5a596e80caad78aa76ee965097d8cb84ad2a7d11
Bennett Leanna <benne3le@student.cs.jmu.edu>
2015-10-13 17:43:26 -0400

    Added print statements.

--- actions.c
+++ actions.c
@@ -8,42 +8,53 @@
 
 void get_payment_method()
 {
-
+        printf("Payment method received.\n");
 }
 
 void dispatch_factory_lines()
 {
-
+	printf("Factory lines dispatched.\n");
 }
 
 
 void shut_down_factory_lines()
 {
-
+        printf("Factory lines shut down.\n");
 }
 
 void get_address()
 {
-
+        printf("Address received.\n");
 }
 
 void update_stats(stats status)
 {
-
+	if (status == DONE)
+	{
+		printf("Status is now done.\n");	
+	}
+	else if (status == LOST)
+	{
+		printf("Status is now lost.\n");
+	}
+	else if (status == FAIL)
+	{
+		printf("Status is now fail.\n");
+	}
 }
 
 void charge_client()
 {
-
+	printf("Client is now being charged.\n");
 }
 
 void start_warranty()
 {
-
+	printf("Warranty has been started.\n");
 }
 
 void refund()
 {
-
+	printf("Refund has been given.\n");
 }
 #endif
