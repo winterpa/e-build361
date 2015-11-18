ebe236879ea0daf8697120cf35074262c9ef0c0a
Bennett Leanna <benne3le@student.cs.jmu.edu>
2015-11-17 19:30:59 -0500

    supervisor.c

--- supervisor.c
+++ supervisor.c
@@ -5,11 +5,22 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <sys/msg.h>
+#include <semaphore.h>
 #include "message.h"
 #include "shmem-ex.h"
 
+struct pro_aggreg{
+	int num_items ; /* Items made during iteration */
+	int iteration ; /* Current iteration */
+} ;
+
+struct pro_aggreg aggregs[n];
+
+
 int main ( int argc , char * argv[] )
 {
+
+
 	/* Initialize the shared memory */
 	int shmid ;
    	key_t shmkey;
@@ -21,7 +32,7 @@ int main ( int argc , char * argv[] )
 
 	shmid = shmget( shmkey , SHMEM_SIZE , shmflg ) ;
 
-	if ( shmid != -1 ) {
+	/*if ( shmid != -1 ) {
 	   printf("\nShared memory segment '0x%X' %s" , shmkey  ,
 		  "successfully created/found with id=%d\n" , shmid ) ;
 	}
@@ -34,61 +45,65 @@ int main ( int argc , char * argv[] )
 	if ( p == (shared_data *) -1 ) {
 	   printf ("\nFailed to attach shared memory id=%d\n" , shmid );
 	   exit(-1) ;
-	}
+	}*/
 
-	int num_lines = atoi(argv[1]);
+	int LinesActive = atoi(argv[1]);
 
 	msgBuf msg ;
 	key_t msgQueKey ;
 	int queID ;
 	int msgStatus ;
 	int result ;
+	int i = 0;
+
+	for(i = 0; i < ActiveLines; i++)
+	{
+		aggregs[i].num_items = msg.info.num_items;
+		aggregs[i].iteration = msg.info.iteration;
+	}
+	
 
 	/* Create / Find the message queues */
 	msgQueKey = BASE_MAILBOX_NAME ;
 	queID = msgget( msgQueKey , IPC_CREAT | 0600 ) ; /*rw. ... ...*/
-	if ( queID < 0 ) {
-		printf("Failed to create mailbox %X. Error code=%d\n", msgQueKey , errno ) ;
-		exit(-2) ;
-	}
-	else
-	printf( "The Calculator process created/found mailbox %X with qid=%d\n", msgQueKey , queID ) ;
-
+	
 	/* Now, wait for a message to arrive from the User process */
-	printf ("\nI am now waiting to receive request ...\n" );
-	msgStatus = msgrcv( queID , &msg , MSG_INFO_SIZE , 1 , 0 );
-	if ( msgStatus < 0 )
-	{
-		printf("Failed to receive message from User process on queuID %d. Error code=%d\n", queID , errno ) ;
-		exit(-2) ;
-	}
-	else
-	{
-		printf("Calculator received the following message from the User:\n" );
-		printMsg( & msg );
-		printf("\n");
-	}
 
-	/* prepare the result to send back to the User process */	
-	msg.info.type
+	sem_wait(&print);
 
-	msg.info.sender = getpid() ;
-
-	/* Send the result message to the User process */
-	msgStatus = msgsnd( queID , &msg , MSG_INFO_SIZE , 0 ) ; /* the msg flag is set to 0 */
-	if ( msgStatus < 0 )
+	while (LinesActive > 0)
 	{
-		printf("Failed to send message to User process on queuID %d. Error code=%d\n", queID , errno ) ;
-		exit(-2) ;
-	}
-	else
-	{
-		printf("Calculator sent the following message to the User:\n" );
-		printMsg( & msg );
-		printf("\n" );
+		msgStatus = msgrcv( queID , &msg , MSG_INFO_SIZE , 1 , 0 );
+		
+		if ( msg.info.type == 1 )
+		{
+			//update production aggregates (num-items-built, num-iterations)
+			aggregs[msg.info.sender].num_items += msg.info.num_items;
+			aggregs[msg.info.sender].iteration++;
+		}
+		else if ( msg.info.type == 2 )
+		{
+			LinesActive --;
+		}
+		else
+		{
+			//discard this unsupported message
+		}
 	}
 
-	printf("\n" );
+	sem_post(&sema);
+
+	sem_wait(&print);
+
+	//Print production aggregates
+	for(i = 0; i < ActiveLines; i++)
+	{
+		printf("Number of Items: %d\n", aggregs[i].num_items);
+		printf("Number of iterations: %d\n", aggregs[i].iteration);
+	} 
+
+	sem_post(&print);
+	
 
 	return 0 ;
 }
@@ -96,9 +111,9 @@ int main ( int argc , char * argv[] )
 /*--------------------------------------------------------------------
 Print a message buffer
 ----------------------------------------------------------------------*/
-void printMsg( msgBuf *m )
+/*void printMsg( msgBuf *m )
 {
 printf( "{ type=%ld , { sender pid=%ld, %3d %c %3d , result=%6d } }\n"
 	, m->mtype, (long) m->info.sender
 	, m->info.num_items, m->info.iteration) ;
-}
+}*/
